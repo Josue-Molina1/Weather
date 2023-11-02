a6764f4691ddd68939e8e462c97fc59d13dbb9e1
Josue Molina <Josue11183@byui.edu>
2023-11-01 23:07:12 -0600

    My computer is burning

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "Globe")
-                .imageScale(.large)
+            Image(systemName: "Car")
+                .imageScale(.small)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Icon")

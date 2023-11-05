0032114a01e6199feeba160315972d0058d517ac
Josue Molina <Josue11183@byui.edu>
2023-11-04 23:40:11 -0600

    some progress on saturday

---
+++ Weather/Managers/LocationManager.swift
@@ -0,0 +1,20 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by Josue Molina on 11/4/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate{
+    let manager = CLLocationManager()
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init(){
+        super.init()
+        manager.delegate = self
+    }
+}
